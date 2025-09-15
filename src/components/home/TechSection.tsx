1fe8179a112fb1b96e98ddc5fc052698eb7754c1
memymini <minhi0614@gmail.com>
2025-09-15 14:26:46 +0900

    chore:skill set 반응형 추가

--- src/components/home/TechSection.tsx
+++ src/components/home/TechSection.tsx
@@ -20,9 +20,15 @@ export default function TechSection({ items }: TechSectionProps) {
         {doubled.map((it, idx) => (
           <li
             key={`${it.name}-${idx}`}
-            className="flex flex-col items-center p-4 glass justify-between gap-4 min-w-25 w-30"
+            className="flex flex-col items-center justify-between gap-4 rounded-2xl p-2 md:p-4 glass min-w-24 w-24 md:w-32 lg:w-40"
           >
-            <Image src={it.src} alt="" width={50} height={50} />
+            <Image
+              src={it.src}
+              alt=""
+              width={50}
+              height={50}
+              className="w-12 h-12 md:w-16 md:h-16 object-contain"
+            />
             <span className="text-xs">{it.name}</span>
           </li>
         ))}
